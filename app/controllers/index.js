683c1cc518937de7e9ec78bbb3d6f1bbca05c483
Gadzhi Gadzhiev <inbox@resure.net>
2017-02-11 01:29:24 +0300

    Fix index controller

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,8 +1,6 @@
-import {query} from '../config/db';
+import db from '../config/db';
 
 export default function (req, res) {
-    query('select 2 + 2 as result')
-        .then((resp) => {
-            res.send({result: resp.rows[0].result});
-        });
+    db.one('select 2 + 2 as result')
+        .then((result) => res.send(result));
 }

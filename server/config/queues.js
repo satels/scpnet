42ff030f37c898252768e076800a13c6f28c1b52
Gadzhi Gadzhiev <inbox@resure.net>
2017-02-11 20:31:43 +0300

    Attach data to sentry events in queues via data attr

--- server/config/queues.js
+++ server/config/queues.js
@@ -6,7 +6,7 @@ const importQueue = queue('Wikidot Data Importer', process.env.REDIS_PORT, '127.
 
 importQueue
     .on('error', (error) => sentry.captureException(error))
-    .on('stalled', (job) => sentry.captureMessage('Job stalled', {level: 'warning', job: job.data}))
-    .on('failed', (job, error) => sentry.captureException(error, {job: job.data}));
+    .on('stalled', (job) => sentry.captureMessage('Job stalled', {level: 'warning', data: {job: job.data}}))
+    .on('failed', (job, error) => sentry.captureException(error, {data: {job: job.data}}));
 
 export {importQueue};

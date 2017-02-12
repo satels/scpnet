612d63346fcc705fdda9494ff24589651a6398a7
Gadzhi Gadzhiev <inbox@resure.net>
2017-02-12 20:11:45 +0300

    Add new common queue with titles extraction as the first action

--- server/config/queues.js
+++ server/config/queues.js
@@ -1,16 +1,22 @@
 'use strict';
 
 require('./boot');
-const queue = require('bull');
+const bull = require('bull');
 const sentry = require('./sentry');
 
-const importQueue = queue('wikidot-import', process.env.REDIS_PORT, '127.0.0.1');
+function addListenersToQueue(queue) {
+    queue
+        .on('error', (error) => sentry.captureException(error))
+        .on('stalled', (job) => sentry.captureMessage('Job stalled', {level: 'warning', extra: {job: job.data}}))
+        .on('failed', (job, error) => sentry.captureException(error, {extra: {job: job.data}}));
 
-importQueue
-    .on('error', (error) => sentry.captureException(error))
-    .on('stalled', (job) => sentry.captureMessage('Job stalled', {level: 'warning', extra: {job: job.data}}))
-    .on('failed', (job, error) => sentry.captureException(error, {extra: {job: job.data}}));
+    return queue;
+}
+
+const importQueue = addListenersToQueue(bull('wikidot-import', process.env.REDIS_PORT, '127.0.0.1'));
+const commonQueue = addListenersToQueue(bull('common-import', process.env.REDIS_PORT, '127.0.0.1'));
 
 module.exports = {
-    importQueue
+    importQueue,
+    commonQueue
 };
--- server/services/worker.js
+++ server/services/worker.js
@@ -1,12 +1,15 @@
 'use strict';
 
-const {importQueue} = require('../config/queues');
+const {importQueue, commonQueue} = require('../config/queues');
 const wk = require('../config/wikidot-kit');
 const sentry = require('../config/sentry');
 const pino = require('../config/pino');
 const importPage = require('../jobs/import-wikidot-page');
+const extractObjectTitles = require('../jobs/extract-object-titles');
 
 pino.info('Import worker ready');
+
+const WIKIDOT_IMPORT_CONCURRENCY = 2;
 
 importQueue.process((job) => {
     const params = job.data;
@@ -17,15 +20,7 @@ importQueue.process((job) => {
             pino.info(`Performing full import from ${params.wiki}`);
             return wk.fetchPagesList({wiki: params.wiki})
                 .then((pages) => {
-                    const queueInserts = pages.map((pageName) => {
-                        return importQueue.add({
-                            action: 'page-import',
-                            wiki: params.wiki,
-                            name: pageName
-                        });
-                    });
                     pino.info(`Full import from ${params.wiki} enqueued`);
-
                     sentry.captureMessage('Full wiki pages import scheduled', {
                         level: 'info',
                         extra: {
@@ -34,15 +29,56 @@ importQueue.process((job) => {
                         }
                     });
 
-                    return queueInserts;
+                    job.progress(`0/${pages.length}`);
+
+                    return pages.map((pageName) => {
+                        return {
+                            name: pageName,
+                            wiki: params.wiki,
+                            total: pages.length
+                        };
+                    });
+                })
+
+                .map(({name, wiki, total}) => {
+                    return importPage({wiki, name}).then(() => {
+                        const current = parseInt(job.progress().split('/')[0], 10) + 1;
+                        return job.progress(`${current}/${total}`);
+                    });
+                }, {concurrency: WIKIDOT_IMPORT_CONCURRENCY})
+
+                .then(() => {
+                    sentry.captureMessage('Full wiki pages import succeeded', {
+                        level: 'info',
+                        extra: {
+                            wiki: params.wiki
+                        }
+                    });
+                    pino.info(`Full import from ${params.wiki} completed`);
+
+                    return 'done';
                 })
                 .catch((error) => {
-                    pino.error(error, 'Error fetching page list during full import', params);
+                    pino.error(error, 'Error occured during full import', params);
                     sentry.captureException(error, {extra: params});
                 });
 
         case 'page-import':
             return importPage({wiki: params.wiki, name: params.name});
+
+        default:
+            pino.error(`Rejecting job with unknown action "${params.action}"`);
+            return Promise.reject(`Unknown job import job type "${params.type}"`);
+    }
+});
+
+commonQueue.process((job) => {
+    const params = job.data;
+
+    switch (params.action) {
+
+        case 'extract-object-titles':
+            return extractObjectTitles();
 
         default:
             pino.error(`Rejecting job with unknown action "${params.action}"`);

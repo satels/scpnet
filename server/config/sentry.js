3496a1838bb41a71bdc7926f21defdee2b2e4539
Gadzhi Gadzhiev <inbox@resure.net>
2017-02-12 03:50:16 +0300

    Pass current package version to the Sentry init

--- server/config/sentry.js
+++ server/config/sentry.js
@@ -2,11 +2,12 @@
 
 const Sentry = require('raven');
 const SENTRY_DSN = process.env.SENTRY_DSN;
+const VERSION = require('../../package.json').version;
 
 let sentry;
 
 if (SENTRY_DSN) {
-    Sentry.config(SENTRY_DSN).install();
+    Sentry.config(SENTRY_DSN, {release: VERSION}).install();
     sentry = Sentry;
 
     sentry.captureException = sentry.captureException.bind(sentry);

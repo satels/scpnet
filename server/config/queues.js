8b7ba8626b193ebede46b4c1853b330f487413ec
Gadzhi Gadzhiev <inbox@resure.net>
2017-02-12 05:08:47 +0300

    Use correct param name for attaching job info on stalled and failed events

--- server/config/queues.js
+++ server/config/queues.js
@@ -8,8 +8,8 @@ const importQueue = queue('wikidot-import', process.env.REDIS_PORT, '127.0.0.1')
 
 importQueue
     .on('error', (error) => sentry.captureException(error))
-    .on('stalled', (job) => sentry.captureMessage('Job stalled', {level: 'warning', data: {job: job.data}}))
-    .on('failed', (job, error) => sentry.captureException(error, {data: {job: job.data}}));
+    .on('stalled', (job) => sentry.captureMessage('Job stalled', {level: 'warning', extra: {job: job.data}}))
+    .on('failed', (job, error) => sentry.captureException(error, {extra: {job: job.data}}));
 
 module.exports = {
     importQueue

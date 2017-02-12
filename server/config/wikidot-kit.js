da7aa77ed1b73b9a58660bfbc9799d077b0e2af1
Gadzhi Gadzhiev <inbox@resure.net>
2017-02-12 23:44:47 +0300

    Update WikidotKit version

--- server/config/wikidot-kit.js
+++ server/config/wikidot-kit.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const WikidotKit = require('wikidot-kit').default;
+const WikidotKit = require('wikidot-kit');
 
 const token = process.env.WIKIDOT_TOKEN;
 const wk = new WikidotKit({token});

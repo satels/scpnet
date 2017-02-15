7cc7f11f2f2e08885aee28c15d1a464b67227e66
Gadzhi Gadzhiev <inbox@resure.net>
2017-02-16 00:37:41 +0300

    Add wk instance to the console context

--- server/console.js
+++ server/console.js
@@ -2,11 +2,12 @@
 
 require('./config/boot');
 const db = require('./config/db');
+const wk = require('./config/wikidot-kit');
 const sentry = require('./config/sentry');
 const queues = require('./config/queues');
 const repl = require('repl');
 
-const context = {db, queues, sentry};
+const context = {db, wk, queues, sentry};
 
 if (require.main === module) {
     const replServer = repl.start();

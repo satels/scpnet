a4f4a42150d53abc1ccccfdc0b8a92be16d12d6b
Gadzhi Gadzhiev <inbox@resure.net>
2017-02-16 00:38:04 +0300

    Add supported wikis list to the wk

--- server/config/wikidot-kit.js
+++ server/config/wikidot-kit.js
@@ -5,4 +5,36 @@ const WikidotKit = require('wikidot-kit');
 const token = process.env.WIKIDOT_TOKEN;
 const wk = new WikidotKit({token});
 
+const WIKIS = {
+    SCP_RU: {
+        name: 'scp-ru',
+        url: 'http://scpfoundation.ru',
+        title: 'SCP Foundation Russia',
+        description: 'Russian branch main wiki'
+    },
+
+    SCP_WIKI: {
+        name: 'scp-wiki',
+        url: 'http://www.scp-wiki.net',
+        title: 'SCP Foundation',
+        description: 'Original English wiki'
+    },
+
+    SCPSANDBOX: {
+        name: 'scpsandbox',
+        url: 'http://sandbox.scpfoundation.ru',
+        title: 'SCP Foundation Russia Sandbox',
+        description: 'Russian branch sandbox wiki'
+    },
+
+    WANDERERS_LIBRARY: {
+        name: 'wanderers-library',
+        url: 'http://wanderers-library.wikidot.com',
+        title: 'The Wanderers\' Library',
+        description: 'Collaborative fiction exercise'
+    }
+};
+
+wk.wiki = WIKIS;
+
 module.exports = wk;

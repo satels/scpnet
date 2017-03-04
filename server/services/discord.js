7d1ce5fe910861cf3645e565fd0094f78b214d6f
Gadzhi Gadzhiev <resure@zireael.org>
2017-03-05 02:26:56 +0300

    Add basic discord bot

---
+++ server/services/discord.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const Discord = require('discord.js');
+const hatbot = new Discord.Client();
+const token = process.env.SCPNET_DISCORD_TOKEN || process.env.DISCORD_TOKEN;
+
+hatbot.on('ready', () => {
+    console.log('Hatbot is ready');
+});
+
+hatbot.on('message', (message) => {
+    if (message.content === 'ping') {
+        message.channel.sendMessage('pong');
+    }
+});
+
+hatbot.login(token);
+
+
